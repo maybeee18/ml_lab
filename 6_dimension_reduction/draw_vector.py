c149472d9b984eae7eeb3432548e0702932fa8e6
Brian Spiering <bspiering@gmail.com>
2019-11-26 17:53:56 -0800

    Add Dimension Reduction and PCA materials

---
+++ 6_dimension_reduction/draw_vector.py
@@ -0,0 +1,6 @@
+def draw_vector(v0, v1, ax=None):
+    ax = ax or plt.gca()
+    arrowprops=dict(arrowstyle='->',
+                    linewidth=2,
+                    shrinkA=0, shrinkB=0)
+    ax.annotate('', v1, v0, arrowprops=arrowprops)
